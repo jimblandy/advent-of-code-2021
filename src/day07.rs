cd91ecffda154c09e06169c8c5c8f790920bdf88
Jim Blandy <jimb@red-bean.com>
2021-12-06 22:49:22 -0800

    Day 7 Part 2.

--- src/day07.rs
+++ src/day07.rs
@@ -23,15 +23,44 @@ fn part1_median(crabs: &[i32]) -> i32 {
     crabs.iter().map(|c| (c - pos).abs()).sum()
 }
 
-#[test]
-fn part1_sample() -> Result<(), ParseIntError> {
-    assert_eq!(part1(&part1_input("16,1,2,0,4,2,7,1,2,14")?),
-               37);
+#[cfg(test)]
+fn sample() -> Vec<i32> {
+    part1_input("16,1,2,0,4,2,7,1,2,14")
+        .expect("error parsing sample input")
+}
 
-    Ok(())
+#[test]
+fn part1_sample() {
+    assert_eq!(part1_median(&sample()), 37);
 }
 
 #[aoc(day7, part1)]
 fn part1(crabs: &Vec<i32>) -> i32 {
     part1_median(crabs)
 }
+
+fn cost(dist: i32) -> i32 { (dist * dist + dist) / 2 }
+
+fn cost_all(pos: i32, crabs: &[i32]) -> i32 {
+    crabs.iter().map(|crab| cost((crab - pos).abs())).sum()
+}
+
+fn part2_mean(crabs: &[i32]) -> i32 {
+    let sum: i32 = crabs.iter().sum();
+    let mean = sum as f32 / crabs.len() as f32;
+    let pos1 = mean.floor() as i32;
+    let pos2 = mean.ceil() as i32;
+    std::cmp::min(cost_all(pos1, crabs),
+                  cost_all(pos2, crabs))
+}
+
+#[test]
+fn part2_sample() {
+    assert_eq!(part2_mean(&sample()), 168);
+}
+
+#[aoc(day7, part2)]
+fn part2(crabs: &Vec<i32>) -> i32 {
+    part2_mean(crabs)
+}
+
