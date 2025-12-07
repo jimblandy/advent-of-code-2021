0c419037db5701715d091875455807ebf0fcdfd8
Jim Blandy <jimb@red-bean.com>
2025-12-06 21:22:47 -0800

    day 7 part 1

---
+++ 2025/day7/src/main.rs
@@ -0,0 +1,94 @@
+#![allow(dead_code, unused_variables)]
+
+use std::{mem, sync::LazyLock};
+
+mod input;
+
+struct Problem {
+    width: usize,
+    start: usize,
+    rows: Vec<Row>,
+}
+
+struct Row {
+    splitters: Vec<usize>,
+}
+
+static TEST_INPUT: LazyLock<Problem> = LazyLock::new(|| Problem::parse(TEST_INPUT_TEXT));
+
+static TEST_INPUT_TEXT: &str = "\
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+";
+
+impl Problem {
+    fn parse(input: &str) -> Self {
+        let mut lines = input.lines();
+        let width = lines.clone().map(|line| line.len()).max().unwrap();
+        let start = lines.next().unwrap().chars().position(|ch| ch == 'S').unwrap();
+        let rows = lines
+            .map(|line| Row {
+                splitters: line.chars()
+                    .enumerate()
+                    .filter(|&(_, ch)| ch == '^')
+                    .map(|(col, _)| col)
+                    .collect()
+            })
+            .collect();
+        Problem { width, start, rows }
+    }
+}
+
+fn part1(problem: &Problem) -> usize {
+    let mut next = Vec::with_capacity(problem.width);
+
+    let mut current = vec![false; problem.width];
+    current[problem.start] = true;
+
+    let mut splits = 0;
+    for row in &problem.rows {
+        next.clone_from(&current);
+        for &splitter in &row.splitters {
+            if current[splitter] {
+                next[splitter - 1] = true;
+                next[splitter] = false;
+                next[splitter + 1] = true;
+                splits += 1;
+            }
+        }
+        mem::swap(&mut current, &mut next);
+    }
+
+    splits
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1(&TEST_INPUT), 21);
+}
+
+fn part2(problem: &Problem) -> usize {
+    todo!()
+}
+
+#[test]
+fn test_part2() {}
+
+fn main() {
+    println!("part 1: {}", part1(&Problem::parse(include_str!("input.txt"))));
+    //part2(&input::INPUT);
+}
