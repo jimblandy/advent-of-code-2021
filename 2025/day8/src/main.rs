7d2fafbc7ec381410967c9ecfcf2626703956433
Jim Blandy <jimb@red-bean.com>
2025-12-07 23:29:09 -0800

    day 8: move test input into a text file

--- 2025/day8/src/main.rs
+++ 2025/day8/src/main.rs
@@ -52,32 +52,9 @@ fn part1(problem: &Problem, num_connections: usize) -> usize {
         .product()
 }
 
-static SAMPLE_INPUT: &str = "\
-162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689
-";
-
 #[test]
 fn test_part1() {
-    assert_eq!(part1(&Problem::parse(SAMPLE_INPUT), 10), 40);
+    assert_eq!(part1(&Problem::parse(include_str!("test_input.txt")), 10), 40);
 }
 
 fn part2(problem: &Problem) -> u64 {
@@ -100,7 +77,7 @@ fn part2(problem: &Problem) -> u64 {
 
 #[test]
 fn test_part2() {
-    assert_eq!(part2(&Problem::parse(SAMPLE_INPUT)), 25272);
+    assert_eq!(part2(&Problem::parse(include_str!("test_input.txt"))), 25272);
 }
 
 fn main() {
